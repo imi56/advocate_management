df3834eb5850a80fb4274ae74edb998fad91b6d4
Imran Ahmad <zaran.56@gmail.com>
2021-07-08 00:43:30 +0530

    Add junior advocate implemented

--- src/advocate.rb
+++ src/advocate.rb
@@ -1,9 +1,15 @@
 class Advocate
-  attr_accessor :name, :juniors, :state, :cases
-  def initialize(name)
+  attr_accessor :name, :juniors, :state, :cases, :junior
+  def initialize(name, junior = false)
     @name = name
     @juniors = []
     @cases = []
     @state = nil
+    @junior = junior
   end
+
+  def print_info(delimeter = "")
+    puts "#{delimeter}Advocate name: #{name}"
+  end
+  
 end
--- src/advocate_manager.rb
+++ src/advocate_manager.rb
@@ -1,3 +1,4 @@
+require 'byebug'
 require_relative './advocate'
 
 
@@ -11,10 +12,17 @@ class AdvocateManager
     print "Enter advocate to be added: "
     advocate_name = gets.strip
     self.advocates << Advocate.new(advocate_name)
-    puts "Advocate #{advocate_name} added"
+    puts "\n*** Advocate #{advocate_name} added ***"
   end
 
   def add_junior_advocate
+    print "Enter senior advocate name: "
+    sr_adv_name = gets.strip
+    sr_adv = find_advocate(sr_adv_name)
+    print "\nEnter junior advocate name: "
+    jr_adv_name = gets.strip
+    sr_adv.juniors << Advocate.new(jr_adv_name, true)
+    puts "Advocate #{jr_adv_name} added under #{sr_adv_name}"
   end
 
   def add_advocate_state
@@ -27,10 +35,30 @@ class AdvocateManager
   end
 
   def display_all_advocates
-
+    puts "*** Listing all advocates below. ***"
+    @advocates.each do |adv|
+      adv.print_info
+      # Printing 1 level nested juniors only
+      adv.juniors.each do |jr_adv|
+        jr_adv.print_info("\t-")
+      end
+    end
   end
 
   def display_all_cases_in_state(state)
+  end
+
+  def find_advocate(adv_name)
+    advs = @advocates.select {|adv| adv.name == adv_name}
+    if advs.size > 1
+      puts "Duplicate advocates found with name #{adv_name}\nexiting..."
+      exit
+    end
+    if advs.size == 0
+      puts "No such advocate found\nexiting..."
+      exit
+    end
+    advs.first
   end
 
 end
